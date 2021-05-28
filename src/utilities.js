c5ec9229f85b8c353143cbc126efabd5fd59f4f0
Alexandr Lezhnin <alezhnin87@gmail.com>
2021-05-28 17:38:17 +0300

    Step 7
    Add function getGCD in src/utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -2,7 +2,15 @@ const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const isEven = (num) => (num % 2 === 0);
 
+const getGCD = (num1, num2) => {
+	if (num1 === num2 || num2 === 0) {
+		return num1;
+	}
+	return getGCD(num2, num1 % num2);
+};
+
 export default {
-  getRandomInt,
-  isEven,
+	getRandomInt,
+	isEven,
+	getGCD,
 };
